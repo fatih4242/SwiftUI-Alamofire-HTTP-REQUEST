a4d99441cd0104159c94db8ec7b9ff11969c2eb9
fatih <fatih16@live.nl>
2022-10-13 16:34:13 +0300

    Initial Commit

---
+++ SwiftUIAlamofire/SwiftUIAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlamofireApp.swift
+//  SwiftUIAlamofire
+//
+//  Created by Fatih Toker on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
